42702ac002c3eb44142afd9d19e0dbe7883ad678
Vincent REHM <contact@vincentrehm.fr>
2020-10-09 02:09:35 +0200

    finish implementation of is owner policy to expense controller

--- api/expense/controllers/expense.js
+++ api/expense/controllers/expense.js
@@ -2,6 +2,47 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
     /**
+     * Retrieve all records based on user.
+     *
+     * @return {Object}s
+     */
+
+    async find(ctx) {
+        let entities;
+
+        ctx.query = {
+            ...ctx.query,
+            owner: ctx.state.user.id,
+        };
+
+        if (ctx.query._q) {
+            entities = await strapi.services.expense.search(ctx.query);
+        } else {
+            entities = await strapi.services.expense.find(ctx.query);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.expense }));
+    },
+
+    /**
+     * Count records based on user.
+     *
+     * @return {Number}
+     */
+
+    count(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            owner: ctx.state.user.id,
+        };
+
+        if (ctx.query._q) {
+            return strapi.services.expense.countSearch(ctx.query);
+        }
+        return strapi.services.expense.count(ctx.query);
+    },
+
+    /**
      * Retrieve a record.
      *
      * @return {Object}
@@ -10,8 +51,16 @@ module.exports = {
     async findOne(ctx) {
         const { slug } = ctx.params;
 
-        const entity = await strapi.services.expense.findOne({ slug });
-        return sanitizeEntity(entity, { model: strapi.models.expense });
+        const expense = await strapi.services.expense.findOne({
+            slug,
+            'owner.id': ctx.state.user.id,
+        });
+
+        if (!expense) {
+            return ctx.unauthorized(`You can't fetch this entry`);
+        }
+
+        return sanitizeEntity(expense, { model: strapi.models.expense });
     },
 
     /**
@@ -61,6 +110,31 @@ module.exports = {
         } else {
             entity = await strapi.services.expense.update({ id }, ctx.request.body);
         }
+
+        return sanitizeEntity(entity, { model: strapi.models.expense });
+    },
+
+    /**
+     * Delete a record.
+     *
+     * @return {Object}
+     */
+
+    async delete(ctx) {
+        const { id } = ctx.params;
+
+        let entity;
+
+        const [expense] = await strapi.services.expense.find({
+            id: ctx.params.id,
+            'owner.id': ctx.state.user.id,
+        });
+
+        if (!expense) {
+            return ctx.unauthorized(`You can't delete this entry`);
+        }
+
+        entity = await strapi.services.expense.delete({ id });
 
         return sanitizeEntity(entity, { model: strapi.models.expense });
     },
